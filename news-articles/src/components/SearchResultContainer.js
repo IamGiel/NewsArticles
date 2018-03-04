0652ed420e582544f07fb74fd4d6ef0aae020ff7
Server <november14giel@gmail.com>
2018-03-03 23:45:20 -0500

    added moment react js - for dates

--- news-articles/src/components/SearchResultContainer.js
+++ news-articles/src/components/SearchResultContainer.js
@@ -34,12 +34,38 @@ class SearchResultContainer extends Component {
   // When the form is submitted, search the Giphy API for `this.state.search`
   handleFormSubmit = event => {
     event.preventDefault();
+    console.log("handleFormSubmit");
     this.searchGiphy(this.state.search);
+  };
+
+  removeArticle = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const articleResults = this.state.results.filter(res => res.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ articleResults });
   };
 
   handleArticleOnClick = event => {
     event.preventDefault();
+    console.log("handleArticleOnClick");
     this.searchGiphy(this.state.url);
+  };
+
+  handleSaveArticle = event => {
+  event.preventDefault();
+  console.log("handleSaveArticle");
+  if (this.state.title && this.state.author) {
+    API.saveBook({
+      title: this.state.title,
+      author: this.state.author,
+      synopsis: this.state.synopsis
+    })
+      .then(res => this.loadBooks())
+      .catch(err => console.log(err));
+  }
+    // event.preventDefault();
+    // console.log("saving article...");
+    // this.searchGiphy(this.state.results);
   };
 
   render() {
@@ -51,7 +77,11 @@ class SearchResultContainer extends Component {
           handleInputChange={this.handleInputChange}
         />
 
-        <Results results={this.state.results} url={this.state.url} />
+        <Results
+          results={this.state.results}
+          url={this.state.url}
+          handleSaveArticle={this.handleSaveArticle}
+        />
       </div>
     );
   }
@@ -62,7 +92,7 @@ export default SearchResultContainer;
 // 1. componentDidMount calls API search query
 // 2. Search Query - Queries the API - returns an array of objects
 // 3. Pass the objects to the state
-// 4. pass the state update the state and
+// 4. pass the state, update the state and
 // 5. pass the state as a prop in the result component (line 48)
 // 6. Inside the prop were mapping through the (prop.result) to the array of object
 // 7. which is why it is being rendered to the browser
---
+++ news-articles/src/utils/mongoApi.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default {
+  // Gets all Articles
+  getArticle: function() {
+    return axios.get("/api/Articles");
+  },
+  // Gets the Article with the given id
+  getArticle: function(id) {
+    return axios.get("/api/Articles/" + id);
+  },
+  // Deletes the Article with the given id
+  deleteArticle: function(id) {
+    return axios.delete("/api/Articles/" + id);
+  },
+  // Saves a Article to the database
+  saveArticle: function(articleData) {
+    return axios.post("/api/Articles", articleData);
+  }
+};
