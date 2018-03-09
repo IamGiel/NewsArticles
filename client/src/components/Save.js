ae1ab8407f7d10fb4f5622ac786f30685e94aaba
Server <november14giel@gmail.com>
2018-03-09 10:46:17 -0500

    data mask

---
+++ client/src/components/Save.js
@@ -0,0 +1,29 @@
+import React from "react";
+// import styles from "../styles/style.css";
+// import Moment from "react-moment";
+
+
+const Saved = props => {
+  return (<div>Saved Searches Here...</div>);
+}
+export default Saved;
+
+/* <button
+  type="button"
+  className="btn-info pull-right"
+  onClick={result.handleSaveArticle}
+>
+  Save{" "}
+</button>; */
+
+// import React  from 'react';
+
+// exports default class MyComponent extends React.Component {
+//     render() {
+//         return (
+//             <Moment format="YYYY/MM/DD">
+//                 1976-04-19T12:59-0500
+//             </Moment>
+//         );
+//     }
+// }
